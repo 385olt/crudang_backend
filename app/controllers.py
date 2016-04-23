5d9b4d0d0e8674d3222b2636d9e89e123601b0d4
Eugen Bronasco <385olt@gmail.com>
2016-04-24 01:10:56 +0300

    first version

--- app/controllers.py
+++ app/controllers.py
@@ -1,16 +1,42 @@
 # coding=utf-8
 
-from flask import render_template, flash, redirect, g
+from flask import jsonify, request
 from app import app
-from .models import getData
+from .models import getData, createData, saveData, deleteData, existsData
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET'])
 def index():
-	data = getData()
-	resData = '['
-	for d in data:
-		resData += '{id: ' + str(d.id) + ', data: \'' + d.data + '\'},'
-	resData += ']'
+	return jsonify({'data': getData()})
 
-	return render_template('index.html', data=resData)
+@app.route('/', methods=['POST'])
+def create():
+	if not request.json or not 'data' in request.json:
+		abort(400)
+	newData = createData(request.json.get('data', ""))
+	return jsonify({'data': newData}), 201
+
+@app.route('/<int:id>', methods=['PUT'])
+def save(id):
+	if not request.json:
+		abort(400)
+	if 'data' in request.json and type(request.json['data']) != str:
+		abort(400)
+	if not existsData(id):
+		abort(404)
+
+	newData = saveData(id, request.json.get('data'))
+
+	return jsonify({'data': newData})
+
+@app.route('/<int:id>', methods=['DELETE'])
+def delete(id):
+	if not existsData(id):
+		abort(404)
+	deleteData(id)
+	return jsonify({'result': True})
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
--- app/models.py
+++ app/models.py
@@ -8,7 +8,36 @@ class Data(db.Model):
 	timestamp = db.Column(db.DateTime)
 
 	def __repr__(self):
-		return '<Data %r>' % (self.data)
+		return '<Data ' + str(self.id) + ' ' + self.data + '>'
+
+def data2dict(data):
+	if isinstance(data, list):
+		return [data2dict(x) for x in data]
+
+	d = dict()
+	d['id'] = data.id
+	d['data'] = data.data
+	return d
+
+def existsData(id):
+	return db.session.query(db.exists().where(Data.id == id)).scalar()
 
 def getData():
-	return Data.query.all()
+	return data2dict(Data.query.all())
+
+def createData(data):
+	data = Data(data=data)
+	db.session.add(data)
+	db.session.commit()
+	return data2dict(Data.query.filter_by(data=data.data).order_by(Data.id.desc()).first())
+
+def saveData(id, data):
+	mydata = Data.query.get(id)
+	mydata.data = data
+	db.session.commit()
+	return data2dict(mydata)
+
+def deleteData(id):
+	mydata = Data.query.get(id)
+	db.session.delete(mydata)
+	db.session.commit()
